ec9b0cefb407085292061ad4fe03f99cf9c17bc1
AlxTafari <kornishin2702@gmail.com>
2026-04-20 01:01:25 +0300

    feat: add pagination, utility function and changePageSize

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -3,20 +3,34 @@ import {CategoryTabs} from "@/widgets/CategoryTabs/CategoryTabs.tsx";
 import {type Category, useGetMoviesByCategoryQuery} from "@/features/movies/api/moviesApi.ts";
 import {useNavigate, useParams} from "react-router-dom";
 import {MovieCard} from "@/entities/movie/ui/MovieCard/MovieCard.tsx";
+import {useState} from "react";
+import {Pagination} from "@/shared/components/Pagination/Pagination.tsx";
 
 export function Category() {
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageSize, setPageSize] = useState(4);
+
     const { category } = useParams<{ category: Category }>()
     const navigate = useNavigate()
-
     const { data } = useGetMoviesByCategoryQuery(
-        { category: category ?? 'popular' },
+        { category: category ?? 'popular', page: currentPage},
         { skip: !category }
     )
 
+
+    const totalPages = data?.total_pages
+
     const onCategoryChange = (category: Category) => {
         navigate(`/category/${category}`)
+        setCurrentPage(1)
     }
+
+    const changePageSizeHandler = (size: number) => {
+        setPageSize(size)
+        setCurrentPage(1)
+    }
+
     if (!category) return <div>Категории куда-то пропали..</div>
 
     return (
@@ -26,12 +40,20 @@ export function Category() {
             </div>
 
             <h2>{category}</h2>
-
+g
             <div className={styles.grid}>
-                {data?.results.slice(0, 6).map(movie => (
+                {data?.results.slice(0, pageSize).map(movie => (
                     <MovieCard key={movie.id} movie={movie} />
                 ))}
             </div>
+            <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                pagesCount={totalPages || 1}
+                pageSize={pageSize}
+                changePageSize={changePageSizeHandler}
+
+            />
         </section>
 
 
